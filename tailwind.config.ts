88817114089d73ee267d31b53a84d39a4305686c
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2024-01-27 19:06:17 -0300

    :tada: init: initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-1": "#231123",
+        "primary-2": "#372554",
+        "primary-3": "#52528c",
+        "primary-4": "#7c9eb2",
+        "primary-black": "#000000",
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
